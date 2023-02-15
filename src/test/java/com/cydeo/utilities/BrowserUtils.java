9eafd0fa3e39390dd039db9934da05202c98cbd3
HristoE <114247445+HristoE@users.noreply.github.com>
2023-02-15 09:32:10 -0500

    review

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,54 +1,80 @@
 package com.cydeo.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.Set;
 
 public class BrowserUtils {
 
+    /*
+      This method will accept int (in seconds)
+      and execute Thread.sleep method for given duration
+      Arg: int second
+       */
+    public static void sleep(int second){
+        second *= 1000;
+        try{
+            Thread.sleep(second);
+        }catch (InterruptedException e){
 
-        /*
-        This method will accept int (in seconds)
-        and execute Thread.sleep method for given duration
-        Arg: int second
-         */
-        public static void sleep(int second){
-            second *= 1000;
-            try{
-                Thread.sleep(second);
-            }catch (InterruptedException e){
+        }
+    }
 
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInURL, String expectedInTitle){
+
+        //Return and store all window handles in a Set.
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInURL )){
+                break;
             }
         }
 
-        public static void switchWindowAndVerify(WebDriver driver, String expectedInURL, String expectedInTitle){
+        //5. Assert:Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
 
-            //Return and store all window handles in a Set.
-            Set<String> allWindowHandles = driver.getWindowHandles();
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+    }
+    public static void verifyTitleContains(WebDriver driver, String expectedInTitle){
+        Assert.assertTrue(driver.getTitle().contains(expectedInTitle));
+    }
 
-            for (String each : allWindowHandles) {
+    /*
+   This method accepts WebElement target,
+   and waits for that WebElement not to be displayed on the page
+    */
+    public static void waitForInvisibilityOf(WebElement target){
+        //Create the object of 'WebDriverWait' class, and set up the constructor args
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
-                driver.switchTo().window(each);
-                System.out.println("Current URL: " + driver.getCurrentUrl());
+        //use the 'wait' object with the proper syntax to create explicit wait conditions.
+        wait.until(ExpectedConditions.invisibilityOf(target));
+    }
 
-                if (driver.getCurrentUrl().contains(expectedInURL )){
-                    break;
-                }
-            }
+    /*
+    This method accepts String title,
+    and waits for that Title to contain given String value.
+     */
+    public static void waitForTitleContains(String title){
+        //Create the object of 'WebDriverWait' class, and set up the constructor args
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
-            //5. Assert:Title contains “Etsy”
-            String actualTitle = driver.getTitle();
-            Assert.assertTrue(actualTitle.contains(expectedInTitle));
-        }
-
-        public static void verifyTitle(WebDriver driver, String expectedTitle){
-            Assert.assertEquals(driver.getTitle(), expectedTitle);
-        }
-        public static void verifyTitleContains(WebDriver driver, String expectedInTitle){
-            Assert.assertTrue(driver.getTitle().contains(expectedInTitle));
-        }
-
+        //use the 'wait' object with the proper syntax to create explicit wait conditions.
+        wait.until(ExpectedConditions.titleContains(title));
     }
 
 
+}
